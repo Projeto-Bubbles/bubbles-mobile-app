456076ea3bf68d804c5082fca5c5f63db0352ff7
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-03-16 15:08:17 -0300

    [chore] setup mobile app with colors and fonts

---
+++ app/src/main/java/com/projects/bubbles/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.projects.bubbles.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Slate950 = Color(0x020617)
+val Slate900 = Color(0x0F172A)
+val Slate100 = Color(0xF1F5F9)
+
+val Zinc700 = Color(0x423F46)
+val Zinc500 = Color(0x71717A)
+val Zinc300 = Color(0xE4E4E4)
+
+val Green = Color(0x0FA070)
+val Red = Color(0xEF4444)
+
+val BlueButton = Color(0xB1C5E1)
