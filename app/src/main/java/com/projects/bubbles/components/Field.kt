ba865aba2987e8ea9fac1eb50daa20780cb44137
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-14 04:31:41 -0300

    [feature]: Adding category selection component and its icons

--- app/src/main/java/com/projects/bubbles/components/Field.kt
+++ app/src/main/java/com/projects/bubbles/components/Field.kt
@@ -1,9 +1,18 @@
 package com.projects.bubbles.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,15 +24,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.projects.bubbles.R
@@ -123,8 +136,41 @@ fun ButtonComponent(value: String, onClick: () -> Unit) {
     }
 }
 
-//@Composable
-//fun CategoryButton(value: String, icon:Painter, onClick: () -> Unit) {
-//
-//}
+@Composable
+fun CategoryButton (
+    icon: Painter,
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(8.dp)
+            .width(50.dp)
+            .height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .clickable(onClick = onClick)
+                .background(color = Color.Gray, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = label,
+                tint = Color.White,
+                modifier = Modifier.size(30.dp)
+            )
+        }
+        Text(
+            text = label,
+            style = TextStyle(fontSize = 12.sp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
 
--- app/src/main/java/com/projects/bubbles/screens/SelectCategory.kt
+++ app/src/main/java/com/projects/bubbles/screens/SelectCategory.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -31,6 +32,7 @@ import com.projects.bubbles.Screen
 import com.projects.bubbles.components.ArrowRight
 import com.projects.bubbles.components.BubbleLogo
 import com.projects.bubbles.components.ButtonComponent
+import com.projects.bubbles.components.CategoryButton
 import com.projects.bubbles.components.NormalText
 import com.projects.bubbles.components.PasswordField
 import com.projects.bubbles.components.TextField
@@ -68,15 +70,76 @@ fun SelectCategory(navController: NavController) {
             Box(
                 Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(top = 80.dp)
+                    .padding(vertical = 16.dp)
             ) {
                 Column(
                     Modifier.padding(32.dp),
                     verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     ArrowRight()
                     TitleText(value = stringResource(id = R.string.category_choice))
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row {
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.sports),
+                        label = "Esporte",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.music),
+                        label = "Música",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.games),
+                        label = "Games",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.art),
+                        label = "Arte",
+                        onClick = {}
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row {
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.technology),
+                        label = "Tecnologia",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.science),
+                        label = "Ciência",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.culinary),
+                        label = "Gastronomia",
+                        onClick = {}
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CategoryButton(
+                        icon = painterResource(id = R.mipmap.reading),
+                        label = "Livros",
+                        onClick = {}
+                    )
                 }
             }
         }
