b4a7cf67b1d5a304f5e2ee5ac6c2c405af4cfa53
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-28 00:44:25 -0300

    [test]: testing logic for dynamic components

--- app/src/main/java/com/projects/bubbles/screens/Feed.kt
+++ app/src/main/java/com/projects/bubbles/screens/Feed.kt
@@ -9,8 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +33,8 @@ import com.projects.bubbles.components.PostBox
 
 @Composable
 fun Feed(navController: NavController) {
+    val posts = remember { mutableStateListOf<String>() }
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -44,7 +51,8 @@ fun Feed(navController: NavController) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(600.dp)
-                    .padding(start = 28.dp, end = 28.dp),
+                    .padding(start = 28.dp, end = 28.dp)
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Row(
@@ -65,12 +73,18 @@ fun Feed(navController: NavController) {
 
                 Spacer(modifier = Modifier.height(32.dp))
 
-                PostBox(
-                    userName = "Paulo Alvares",
-                    userUsername = "paulinhoAl",
-                    postTime = "2 hours ago",
-                    commentContent = "Lorem ipsum dolor sit amet consectetur. In dolor porttitor malesuada sit et. Amet enim iaculis gravida nulla egestas ultrices phasellus consequat. Eget mauris in lacus risus porttitor."
-                )
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    /* items(posts) { post ->
+                         PostBox(
+                            userName = post.userName,
+                            userUsername = post.userUsername,
+                            postTime = post.postTime,
+                            commentContent = post.commentContent
+                        )
+                   } */
+                }
             }
 
             NavigationBar("feed")
