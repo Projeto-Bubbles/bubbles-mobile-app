e8c6a79fd665f9940c3adcdd8cad25bd9e3b0a82
Paulo-Alvares <paulo.alvares@sptech.school>
2024-04-28 00:30:11 -0300

    [test]: refactoring use of textField

--- app/src/main/java/com/projects/bubbles/components/Field.kt
+++ app/src/main/java/com/projects/bubbles/components/Field.kt
@@ -1,10 +1,20 @@
 package com.projects.bubbles.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -16,18 +26,25 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.projects.bubbles.R
+import com.projects.bubbles.screens.Feed
 import com.projects.bubbles.ui.theme.rounded
 
 @Composable
@@ -103,21 +120,29 @@ fun PasswordField(
 
 @Composable
 fun ResponseField(onValueChange: (String) -> Unit) {
-    OutlinedTextField(
-        onValueChange = onValueChange,
+    Row (
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(10.dp),
-        value = "Responder",
-        leadingIcon = {
-            Icon(
-                painter = painterResource(id = R.mipmap.response_arrow), contentDescription = null
-            )
-        },
-        colors = TextFieldDefaults.colors(
-            unfocusedContainerColor = Color.White,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+            .height(30.dp)
+            .background(Color.White,
+                shape = RoundedCornerShape(8.dp))
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            painter = painterResource(id = R.mipmap.response_arrow),
+            contentDescription = null,
+            modifier = Modifier
+                .size(30.dp)
         )
-    )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        BasicTextField(
+            value = "",
+            onValueChange = onValueChange,
+            textStyle = TextStyle.Default.copy(color = Color.Black),
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
 }
